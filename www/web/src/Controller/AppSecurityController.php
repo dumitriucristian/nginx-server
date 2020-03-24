ce3b7f1a737f8f961f18a71da0e592eca28bc713
cristian <dumitriucristian@yahoo.com>
2020-03-24 08:54:35 +0200

    add auth

--- www/web/src/Controller/AppSecurityController.php
+++ www/web/src/Controller/AppSecurityController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class AppSecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
