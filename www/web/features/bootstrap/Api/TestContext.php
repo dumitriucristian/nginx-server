03a20a1e84e0459092218a74597222a5017a1dd3
cristian <dumitriucristian@yahoo.com>
2020-05-11 16:40:30 +0300

    first request

--- www/web/features/bootstrap/Api/TestContext.php
+++ www/web/features/bootstrap/Api/TestContext.php
@@ -1,11 +1,21 @@
 <?php
 
+
+
 namespace Api;
 
+use Symfony\Component\HttpFoundation\Request;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Mink\Session;
+use Behat\Behat\Tester\Exception\PendingException;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\BrowserKit;
+use Symfony\Component\HttpClient\HttpClient;
 
 /**
  * Defines application features from the specific context.
@@ -19,8 +29,17 @@ class TestContext implements Context
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
-    public function __construct(KernelInterface $kernel)
+
+    private $kernel;
+    private $session;
+    private $browser;
+
+
+    public function __construct(KernelInterface $kernel, Session $session)
     {
+        $this->session = $session;
+        $this->kernel = $kernel;
+
     }
 
     /**
@@ -28,6 +47,22 @@ class TestContext implements Context
      */
     public function aDemoScenarioSendsARequestTo($arg1)
     {
+       $request = Request::create(
+            '/api/login_check',
+            'POST',
+            [],
+            [],
+            [],
+            [
+                'SERVER_PORT' => 8080,
+                'CONTENT_TYPE' => 'application/json'
+            ],
+            json_encode(["username"=>"test","password"=>"test"])
+        );
+
+        $response = $this->kernel->handle( $request );
+
+       dd($response);
         throw new PendingException();
     }
 
